77ea25c02625db6b8664b510d1bbcf42590d2956
GamingAstronamy <ethan-barnes@hotmail.com>
2022-03-27 01:43:28 -0500

    Relicense

--- sample/train.py
+++ sample/train.py
@@ -1,3 +1,4 @@
+    # train.py -- Creates a new GAN and trains it on the MNIST dataset
     # Copyright (C) 2022  Ethan Barnes
 
     # This program is free software: you can redistribute it and/or modify
@@ -13,5 +14,23 @@
     # You should have received a copy of the GNU Affero General Public License
     # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import tensorflow as tf
+from helpers import GAN
+
+
+def main():
+    tf.random.set_seed(42)
+    
+    gan = GAN(100, 256, 2, (5,5), (28, 28, 1))
+
+    (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+    train_images = (train_images - 127.5) / 127.5
+    
+    gan.train(train_images, 1)
+    gan.save('model/')
+
+if __name__ == '__main__':
+    main()
